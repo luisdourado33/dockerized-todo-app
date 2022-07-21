4476ccdcc602120e49a7dd31900c8a1706ca7b49
Luis Dourado <luis_dourado33@hotmail.com>
2022-07-21 14:03:55 -0400

    feat: eslint-disabled exception add

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import IPage from "../../interfaces/page";
 
-const HomePage: React.FC<IPage> = ({ name }) => {
+const HomePage: React.FC<IPage> = () => {
   return (
     <div>
       <h1>Homepage</h1>
