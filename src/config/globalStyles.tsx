9392416bba89350e459f89089b045ffc46aed3a0
Luis Dourado <luis_dourado33@hotmail.com>
2022-07-25 09:08:22 -0400

    chore: font family setup

--- src/config/globalStyles.tsx
+++ src/config/globalStyles.tsx
@@ -1,8 +1,16 @@
 import { createGlobalStyle, GlobalStyleComponent } from "styled-components";
 
+const Lato = "../assets/fonts/Lato/Lato-Regular.ttf";
+
 const GlobalStyles: GlobalStyleComponent<any, any> = createGlobalStyle`
   * {
-    //font-family: JetBrains Mono, sans-serif;
+    font-family: "Lato", sans-serif;
+  }
+
+  @font-face {
+    font-family: "Lato";
+    src: local('Lato'), local('Lato'),
+    url(${Lato}) format('ttf')
   }
 `;
 
