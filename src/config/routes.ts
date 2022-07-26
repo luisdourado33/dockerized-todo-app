4c53bbab2a308519b2b3a4557dbf0ba46a49e454
Luis Dourado <luis_dourado33@hotmail.com>
2022-07-26 12:20:25 -0400

    feat: configure dashboard route

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -3,10 +3,12 @@ import IRoute from "../interfaces/routes";
 
 import HomePage from "../components/pages/home/home";
 import LoginPage from "../components/pages/login/login";
+import Dashboard from "../components/pages/dashboard/dashboard";
 
 const routes: IRoute[] = [
   { path: "/", name: "Homepage", element: HomePage, exact: true },
   { path: "/login", name: "Login", element: LoginPage, exact: true },
+  { path: "/dashboard", name: "Dashboard", element: Dashboard, exact: true },
 ];
 
 export default routes;
